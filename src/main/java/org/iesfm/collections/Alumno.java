f3254b2473c6516add44cd577795d7170a8c1da7
mikelsanvi <mikelsanvi@gmail.com>
2021-10-18 15:05:18 +0200

    repaso mapas

---
+++ src/main/java/org/iesfm/collections/Alumno.java
@@ -0,0 +1,42 @@
+package org.iesfm.collections;
+
+import java.util.Objects;
+
+public class Alumno {
+    private String nombre;
+    private String apellidos;
+
+    public Alumno(String nombre, String apellidos) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Alumno alumno = (Alumno) o;
+        return Objects.equals(nombre, alumno.nombre) && Objects.equals(apellidos, alumno.apellidos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, apellidos);
+    }
+}
--- src/main/java/org/iesfm/collections/ListMethods.java
+++ src/main/java/org/iesfm/collections/ListMethods.java
@@ -1,6 +1,6 @@
 package org.iesfm.collections;
 
-import java.util.List;
+import java.util.*;
 
 public class ListMethods {
 
@@ -16,5 +16,29 @@ public class ListMethods {
         }
 
         return suma / numeros.size();
+    }
+
+    public static void main(String[] args) {
+        List<Double> numeros = new ArrayList<>();
+        numeros.add(4d);
+        numeros.add(5d);
+        numeros.add(1d);
+        // List(4, 5, 1)
+
+        Map<Alumno, Integer> notasPorUsuario = new HashMap<>();
+        notasPorUsuario.put(new Alumno("Ana Luz", "Avilés"), 7);
+        notasPorUsuario.put(new Alumno("Michal", "Homenda"), 6);
+        notasPorUsuario.put(new Alumno("Otro", "Alumno"), 4);
+
+        notasPorUsuario.keySet(); // Set(Alumno(AnaLuz), Alumno(Michal))
+        notasPorUsuario.values(); // List(7, 6)
+        double totalNotas = 0;
+        for (Integer nota : notasPorUsuario.values()) {
+            totalNotas += nota;
+        }
+
+        double media = totalNotas / notasPorUsuario.size();
+
+        System.out.println(media);
     }
 }
